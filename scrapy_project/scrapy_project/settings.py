f6cd2fdbaa093b6b28f84ff2691e3f7382acf45a
Adrian Bergem <adrian@bergem.com>
2020-01-30 09:33:10 +0100

    Added selenium functionality for clicking on button to access more data; Changed settings to limit logging to INFO

--- scrapy_project/scrapy_project/settings.py
+++ scrapy_project/scrapy_project/settings.py
@@ -9,11 +9,11 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'scrapy_project'
+BOT_NAME = 'Haimato'
 
 SPIDER_MODULES = ['scrapy_project.spiders']
 NEWSPIDER_MODULE = 'scrapy_project.spiders'
-
+LOG_LEVEL = 'INFO'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'scrapy_project (+http://www.yourdomain.com)'
--- scrapy_project/scrapy_project/spiders/finn_spider.py
+++ scrapy_project/scrapy_project/spiders/finn_spider.py
@@ -2,7 +2,9 @@
 
 
 import scrapy
+from selenium import webdriver
 import re
+import logging
 
 class FinnSpider(scrapy.Spider):
     name = 'Finn'
@@ -10,7 +12,11 @@ class FinnSpider(scrapy.Spider):
         'https://www.finn.no/realestate/homes/search.html?location=0.20061'
     ]
 
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+
     def parse(self, response):
+        logging.info(f'Crawling {response.url}')
         # follow links to each ad
         # for href in response.css('article.ads__unit a::attr(href)').getall():
         for href in [response.css('article.ads__unit a::attr(href)').get()]:
@@ -18,18 +24,23 @@ class FinnSpider(scrapy.Spider):
                 yield response.follow(href, callback=self.parse_ad)
 
         next_page = response.xpath('//a[@aria-label="Neste side"]/@href').get()
-        print(next_page)
 
         if next_page:
             yield response.follow(next_page, callback=self.parse)
 
-    def parse_ad(sef, response):
+    def parse_ad(self, response):
 
         def extract_digits(string):
             return re.sub('\D', '', string)
 
+        self.driver.get(response.url)
+        logging.info(f'Scraping data from {response.url}')
+        expand_data_button = self.driver.find_element_by_xpath('//button[@id="_expand"]')
+        expand_data_button.click()
         d = {}
-        for i, _ in enumerate(response.xpath(f'//div[@class="panel"]//dl[@class="definition-list"]/dt').getall()):
+        d['finn_code'] = response.url.split('=')[1]
+        
+        for i, _ in enumerate(response.xpath('//div[@class="panel"]//dl[@class="definition-list"]/dt').getall()):
             field = response.xpath(f'//div[@class="panel"]//dl[@class="definition-list"]/dt[{i+1}]/text()').get()
             value = extract_digits(response.xpath(f'//div[@class="panel"]//dl[@class="definition-list"]/dd[{i+1}]').get())
             if field == 'Omkostninger':
@@ -41,6 +52,21 @@ class FinnSpider(scrapy.Spider):
             elif field == 'Fellesgjeld':
                 d['joint_debt'] = value
 
+        for i, _ in enumerate(response.xpath('//div[@data-controller="moreKeyInfo"]//dl[@class="definition-list"]/dt').getall()):
+            field = response.xpath(f'//div[@data-controller="moreKeyInfo"]//dl[@class="definition-list"]/dt[{i}]/text()').get()
+            value = response.xpath(f'//div[@data-controller="moreKeyInfo"]//dl[@class="definition-list"]/dd[{i}]/text()').get()
+            
+            if field == 'Rom':
+                d['no_of_rooms'] = value
+            elif field == 'Tomteareal':
+                d['lot_space'] = extract_digits(value)
+            elif field == 'Bruttoareal':
+                d['floor_space'] = extract_digits(value)
+            elif field == 'Fellesformue':
+                d['joint_capital'] = value
+            elif field == 'Formuesverdi':
+                d['asset_value'] = value
+            
             d['address'] = response.xpath('//section[@class="panel"]//p[@class="u-caption"]/text()').get()
             d['price'] = extract_digits(response.xpath('//div[@class="panel"]//span[@class="u-t3"]/text()').get())
             d['type'] = response.xpath('//section[@class="panel"][2]//dd/text()').get()

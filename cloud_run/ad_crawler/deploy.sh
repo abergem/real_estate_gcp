30f997fcbf03c5edf693de4af490ae1484040874
Adrian Bergem <adrian@bergem.com>
2020-02-13 10:24:37 +0100

    Updated to private GCP;

--- cloud_run/ad_crawler/deploy.sh
+++ cloud_run/ad_crawler/deploy.sh
@@ -1,8 +1,9 @@
 export GRUN_REGION='europe-west1'
-export GOOGLE_CLOUD_PROJECT='acn-dai-nordic'
+export GOOGLE_CLOUD_PROJECT='advance-nuance-248610'
+export TAG='adcrawler'
 
-gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/ad_crawler
-gcloud beta run deploy ad_crawler --image gcr.io/$GOOGLE_CLOUD_PROJECT/ad_crawler --region $GRUN_REGION --platform managed
+gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/$TAG
+gcloud run deploy adcrawler --image gcr.io/$GOOGLE_CLOUD_PROJECT/$TAG:latest --region $GRUN_REGION --platform managed
 
 # gcloud functions deploy run_spider --runtime python37 \
 #                                        --memory 1024MB \
